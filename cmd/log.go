91ad75d81daf771c7affc58bef66e4051361cdbf
kumneger0 <kumnegerwondimu01@gmail.com>
2026-01-28 23:13:25 +0300

    fix(log): log command using hard coded path

--- cmd/log.go
+++ cmd/log.go
@@ -3,18 +3,31 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"runtime"
 
+	"github.com/kumneger0/clispot/internal/config"
 	"github.com/spf13/cobra"
 )
 
 func clispotLog() *cobra.Command {
+	userConfig := config.GetUserConfig(runtime.GOOS)
 	return &cobra.Command{
+
 		Use:          "log",
 		Short:        "show log files use it for error reporting",
 		Args:         cobra.NoArgs,
 		SilenceUsage: true,
 		Run: func(cmd *cobra.Command, args []string) {
-			logs, err := os.ReadFile("/tmp/clispot.log")
+			if userConfig == nil {
+				fmt.Println("No user config found.")
+				return
+			}
+			if userConfig.DebugDir == nil {
+				fmt.Println("No debug directory configured.")
+				return
+			}
+			logs, err := os.ReadFile(filepath.Join(*userConfig.DebugDir, "clispot.log"))
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "Error reading log file: %v", err)
 				os.Exit(1)

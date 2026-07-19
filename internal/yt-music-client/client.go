4e436a0c74bbee56cfecd644aec3c49483b2aa76
kumneger0 <kumnegerwondimu01@gmail.com>
2026-07-19 21:59:54 +0300

    fix: dont use flag parse

--- internal/yt-music-client/client.go
+++ internal/yt-music-client/client.go
@@ -1,20 +1,13 @@
 package ytmusicclient
 
 import (
-	"flag"
-
 	musicpb "github.com/kumneger0/clispot/gen"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-var (
-	addr = flag.String("addr", "localhost:50051", "the address to connect to")
-)
-
-func GetYtMusicClient() (musicpb.MusicServiceClient, *grpc.ClientConn, error) {
-	flag.Parse()
-	conn, err := grpc.NewClient(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+func GetYtMusicClient(addr string) (musicpb.MusicServiceClient, *grpc.ClientConn, error) {
+	conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		return nil, nil, err
 	}
